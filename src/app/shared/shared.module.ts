0552624ad7ec776e50c87dc4e21cdef363a0ffb4
kylqin <kylqin@outlook.com>
2020-09-02 18:43:04 +0800

    add ng-zorro-antd

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,10 +6,12 @@ import { TranslateModule } from '@ngx-translate/core';
 import { PageNotFoundComponent } from './components/';
 import { WebviewDirective } from './directives/';
 import { FormsModule } from '@angular/forms';
+import { NgZorroAntdModule } from './ng-zorro-antd.module';
+import { IconModule } from './ng-zorro-antd-icon.module';
 
 @NgModule({
   declarations: [PageNotFoundComponent, WebviewDirective],
-  imports: [CommonModule, TranslateModule, FormsModule],
-  exports: [TranslateModule, WebviewDirective, FormsModule]
+  imports: [CommonModule, TranslateModule, FormsModule, NgZorroAntdModule, IconModule],
+  exports: [TranslateModule, WebviewDirective, FormsModule, NgZorroAntdModule, IconModule]
 })
 export class SharedModule {}
